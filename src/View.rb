a5e10319105b900473e28f1a20555381c6471a60
hnagamin <1995.hnagamin@gmail.com>
2014-10-12 10:01:44 +0900

    GunView, LifeView, EnemiesViewを実装した
    書いただけでメインルーチンには取り入れていない

---
+++ src/View.rb
@@ -0,0 +1,100 @@
+class GunView
+  def initialize(controller, gun)
+    @controller = controller
+    @gun = gun
+    @gun.registerObserver(self)
+    @width = keta(@gun.maximum)
+  end
+
+  def to_s
+    @view
+  end
+
+  def create_view
+    @view = "_" * @width
+  end
+
+  def update_bullet
+    orderfixed = OrderFixed.new(@gun.bullet, @width)
+    @view = orderfixed.to_s("_")
+  end
+
+  def update_status
+    if @gun.inavailable?
+      @view = "#" * 3
+    elsif @gun.broken?
+      @view = "*" * 3
+    elsif @gun.available?
+      update_bullet
+    end
+  end
+
+  def insert_bullet(num)
+    @controller.insert_bullet(num)
+  end
+
+  def shoot_bullet
+    @controller.shoot
+  end
+end
+
+
+class LifeView
+  def initialize(life)
+    @life = life
+    @life.register_observer(self)
+    @width = keta(@life.maximum)
+  end
+
+  def to_s
+    @view
+  end
+
+  def concatnate(life)
+    orderfixed = OrderFixed.new(life.to_s, @width)
+    "[" + orderfixed.to_s + "%]"
+  end
+
+  def create_view
+    @view = concatnate(@life.life)
+  end
+
+  def update_life
+    create_view
+  end
+end
+
+
+class EnemiesView
+  def initialize(enemies, width)
+    @enemies = enemies
+    @width = width
+  end
+
+  def to_s
+    @view
+  end
+
+  def create_view
+    update_view
+  end
+
+  def update_view
+    ret = ""
+    @enemies.each do |e|
+      expr = e.value.to_s
+      padding_size = e.pos - expr.length - ret.length
+      padding = "_" * (padding_size > 0 ? padding_size : 0)
+      ret += padding + expr
+    end
+
+    if ret.length < @width
+      ret + "_" * (@width - ret.length)
+    elsif ret.length > @width
+      ret[0, @width]
+    else
+      ret
+    end
+  end
+end
+
